f226aafc2c788aff95cbbe607b4d1e19f7b394b6
Rosny Kalend <rosnykalendkab@gmail.com>
2025-07-11 14:23:20 +0200

    Authentification, Gestion des travaux de l'agenda parlementaire

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -30,7 +30,7 @@ export default function LoginPremium() {
                     return;
             }
         }
-    },[isAuthenticate])
+    },[isAuthenticate, navigate, setError, user.role])
 
     // const handleSubmit = async (e) => {
     //     e.preventDefault();
